2ea9a889a5fae36a4968ee942c3e3f48aebced28
Edwards0025 <129787412+Edwards67@users.noreply.github.com>
2023-11-29 20:45:48 +0000

    more

--- src/config.mjs
+++ src/config.mjs
@@ -2,7 +2,7 @@ export default {
     title: "Homepage",
     titleSuffix: " | Edwards ☕",
     description: "Great space to look at the retro games review",
-    defaultOgImage: "./defaultog.jpg",
+    defaultOgImage: "./homepage.jpg",
     latestPosts: 3,
     postsPerPage: 3,
     url: "https://pacamara-astro-6y7xr.kinsta.page",
